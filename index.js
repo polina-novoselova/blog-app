e0bce73443896292a77a7de9d8ca73956d264b60
Polina Novoselova <polka.nov@mail.ru>
2023-06-05 23:46:04 +0200

    add events keydown end events for inputs

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const POPUP_ADD_POST_OPEN_CLASSNAME = "js-popup-open";
 
 const bodyNode = document.querySelector("body");
 
-const popupAddPostBtnOpenLgNode = document.getElementById("add-post-btn-open-lg");
-const popupAddPostBtnOpenSmNode = document.getElementById("add-post-btn-open-sm");
+const popupAddPostBtnOpenLgNode = document.getElementById(
+  "add-post-btn-open-lg"
+);
+const popupAddPostBtnOpenSmNode = document.getElementById(
+  "add-post-btn-open-sm"
+);
 const popupAddPostBtnCloseNode = document.getElementById("add-post-btn-close");
 const popupAddPostNode = document.getElementById("popup-add-post");
 const popupContentNode = document.getElementById("popup-add-post-content");
@@ -35,7 +39,7 @@ popupAddPostBtnCloseNode.addEventListener("click", clearInput);
 function toggleClassNamePopup() {
   popupAddPostNode.classList.toggle(POPUP_ADD_POST_OPEN_CLASSNAME);
   bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
-};
+}
 
 //Закрытие попапа вне поля контента
 
@@ -60,8 +64,8 @@ function publishPost() {
 
   //проверка на наличие значение в инпутах
   if (postTitleFromUser === "" || postDscrptFromUser === "") {
-        return;
-  };
+    return;
+  }
 
   // сохранить пост
   setPost(postFromUser);
@@ -77,7 +81,7 @@ function publishPost() {
 };
 
 //Делаем счетчики знаков в инпутах
-postTitleInputNode.addEventListener('input', onInputTitle);
+postTitleInputNode.addEventListener("input", onInputTitle);
 
 function onInputTitle(event) {
   const length = event.target.value.length;
@@ -85,7 +89,7 @@ function onInputTitle(event) {
   counterPostTitleNode.textContent = length;
 };
 
-postDscrptInputNode.addEventListener('input', onInputDscrpt);
+postDscrptInputNode.addEventListener("input", onInputDscrpt);
 
 function onInputDscrpt(event) {
   const length = event.target.value.length;
@@ -93,14 +97,54 @@ function onInputDscrpt(event) {
   counterPostDscrptNode.textContent = length;
 };
 
-//Опубликовать по нажатию на ентер
-postDscrptInputNode.addEventListener('keydown', publishPostOnEnter);
+//Перенос строки при нажатии контрл+ентер и публикация при нажатии ентер
 
-function publishPostOnEnter(event) {
-  if (event.code === 'Enter') {
-    publishBtnNode.click();
+// Добавляем обработчик события "keydown" на поле ввода описания поста
+postDscrptInputNode.addEventListener("keydown", handlerKeyDown);
+
+// Обработчик события "keydown"
+function handlerKeyDown(event) {
+  // Проверяем, была ли нажата клавиша Enter и клавиша Ctrl
+  if (event.key === "Enter" && event.ctrlKey) {
+    event.preventDefault();
+    insertLineBreak(); // Вставляем символ новой строки
+  } else if (event.key === "Enter" && !event.ctrlKey) {
+    event.preventDefault();
+    publishPost(); // Публикуем пост
   }
 };
+
+postTitleInputNode.addEventListener("keydown", keyDownEnter);
+
+function keyDownEnter(event) {
+  if (event.key === "Enter" && !event.ctrlKey) {
+    event.preventDefault();
+    publishPost();
+  }  
+};
+
+// Функция для вставки символа новой строки
+function insertLineBreak() {
+  const { selectionStart, selectionEnd, value } = postDscrptInputNode;
+  // Разбиваем объект `postDscrptInputNode` на отдельные переменные:
+  // `selectionStart` - позиция начала выделенного текста
+  // `selectionEnd` - позиция конца выделенного текста
+  // `value` - значение текста в поле ввода
+
+  const lineBreak = "\n"; // Создаем переменную `lineBreak`, которая содержит символ новой строки
+
+  // Создаем новую строку `newValue`, объединяя три части:
+  // 1. Строка до начала выделенного текста
+  // 2. Символ новой строки
+  // 3. Строка после выделенного текста
+  const newValue = value.slice(0, selectionStart) + lineBreak + value.slice(selectionEnd);
+
+  postDscrptInputNode.value = newValue; // Устанавливаем новое значение текста в поле ввода
+
+  // Устанавливаем позицию курсора на одну позицию после символа новой строки,
+  // чтобы курсор перешел на новую строку
+  postDscrptInputNode.setSelectionRange(selectionStart + 1, selectionStart + 1);
+};  
 
 //Получаем значения из инпутов
 function getPostFrormUser() {
@@ -125,15 +169,23 @@ function clearInput() {
 function renderPost() {
   let postsListHTML = "";
 
+  // Добавляем новый пост в массив posts
   posts.push({
     postTitleFromUser: postTitleFromUser,
     postDscrptFromUser: postDscrptFromUser,
   });
 
+  // Проходимся по каждому посту в массиве posts
   posts.forEach((post) => {
-    const {postTitleFromUser, postDscrptFromUser} = post;
-    postsListHTML += `<li class="post-item"><h3 class="post-title">${postTitleFromUser}</h3><p class="post-text"> ${postDscrptFromUser}</p></li>`;
+    const { postTitleFromUser, postDscrptFromUser } = post;
+
+    // Заменяем символы новой строки на <br> в описании поста
+    const formattedPostDscrpt = postDscrptFromUser.replace(/\n/g, "<br>");
+
+    // Формируем HTML-код для каждого поста и добавляем его в переменную postsListHTML
+    postsListHTML += `<li class="post-item"><h3 class="post-title">${postTitleFromUser}</h3><p class="post-text">${formattedPostDscrpt}</p></li>`;
   });
 
+  // Вставляем сформированный HTML-код в элемент с идентификатором postsListNode
   postsListNode.innerHTML = `<ul class="posts-list">${postsListHTML}</ul>`;
 };
